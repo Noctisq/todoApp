4b7e6623e24b09e0d64ffff0d4d892e7bf436ebe
Noctisq <noctisumire@gmail.com>
2021-02-05 13:07:48 -0600

    Completed

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,38 +1,124 @@
-function addTask() {
+// Task prototype 
 
-    //Tomar value y limpiar input
-    const task = document.querySelector("#task").value;
-    document.querySelector("#task").value = "";
-    
-    console.log(task);
+function Task(id, desc, completed) {
+
+    this.id = id;
+    this.desc = desc;
+    this.completed = completed;
+}
+
+// functions
 
 
+function getTasks() {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    return tasks;
+}
+
+function updateTasks(newTasks) {
+    localStorage.setItem("tasks", JSON.stringify(newTasks));
+    printTasks(newTasks);
+}
+
+function printTasks(tasks) {
+    document.querySelector("#taskDiv").innerHTML = "";
+    tasks.forEach(task => {
+        console.log(task)
+        if (task.completed) {
+            document.querySelector("#taskDiv").innerHTML += `<p id = "#task${task.id}" style = "text-decoration: line-through"> <input type="checkbox" class = "check" checked>${task.desc} <button class = "Delete">delete</button></p>`;
+        } else {
+            document.querySelector("#taskDiv").innerHTML += `<p id = "#task${task.id}"> <input type="checkbox" class = "check" >${task.desc} <button class = "Delete">delete</button></p>`;
+        }
+
+
+    });
+
+    document.querySelectorAll(".Delete").forEach(btn => {
+        btn.addEventListener('click', deleteTask);
+    });
+    document.querySelectorAll(".check").forEach(input => {
+        input.addEventListener("change", underlineTask)
+    });
 
 
 }
 
+function addTask() {
+
+    let tasks = getTasks();
+    const taskdesc = document.querySelector("#task").value;
+    if (!taskdesc.trim()) {
+        return alert('add a task');
+    }
+    const task = new Task(tasks.length, taskdesc, false);
+    tasks.push(task);
+    updateTasks(tasks);
+    document.querySelector("#task").value = "";
+    printTasks(tasks);
+
+}
+
+function deleteTask(btn) {
+    console.log(btn.srcElement.parentNode.id);
+    const tasks = getTasks();
+    const filteredTasks = tasks.filter(task => `#task${task.id}` != btn.srcElement.parentNode.id);
+    console.log(filteredTasks);
+    updateTasks(filteredTasks);
+
+}
+
+
+function underlineTask(input) {
+
+
+
+    let tasks = getTasks().map(task => {
+
+        if (input.target.parentNode.id == `#task${task.id}`) {
+            task.completed = !task.completed;
+        }
+        return task;
+    });
+
+    updateTasks(tasks);
+
+}
 
 function makeUI() {
 
+    let tasks = [];
+
+
     // Crear elementos para pintar UI
     const newDiv = document.createElement("div");
+    const tasksDiv = document.createElement("div");
+
     const inputTask = document.createElement("input");
     const appDiv = document.querySelector("#app");
     const btnApp = document.createElement("button");
 
-    // Boton para agregar tareas
+    tasksDiv.id = "taskDiv"
     btnApp.textContent = "Add";
     btnApp.id = "add";
-
-    // Input para las tareas
     inputTask.id = "task";
     newDiv.appendChild(inputTask);
     newDiv.appendChild(btnApp);
     appDiv.appendChild(newDiv);
-
+    appDiv.appendChild(tasksDiv);
 
     btnApp.addEventListener("click", addTask);
+
+
+    if (!localStorage.hasOwnProperty('tasks')) {
+        updateTasks(tasks);
+
+    } else {
+        printTasks(getTasks());
+    }
 }
 
 
 makeUI();
+
+
