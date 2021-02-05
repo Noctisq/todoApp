f8912082a671291cd5b0770fb10fb0fb784b4031
[R2D2] <44850953+Arthur-R2D2@users.noreply.github.com>
2021-02-05 17:57:26 -0600

    Creando las funciones updateTasks y printTasks

--- src/JS/index.js
+++ src/JS/index.js
@@ -11,6 +11,35 @@ function Task(id, description, button, completed) {
 
 // Functions
 
+// Function Get Tasks
+
+
+// Function Update Tasks
+function updateTasks(newTasks) {
+    localStorage.setItem("tasks", JSON.stringify(newTasks));
+    printTasks(newTasks);
+}
+
+// Function Print Tasks
+function printTasks(tasks) {
+    document.querySelector("#taskDiv").innerHTML = "";
+    tasks.forEach(task => {
+        console.log(task)
+        if (task.completed) {
+            document.querySelector("#taskDiv").innerHTML += `<p id = "#task${task.id}" style = "text-decoration: line-through"> <input type="checkbox" class = "check" checked>${task.description} <button class = "Delete">delete</button></p>`;
+        } else {
+            document.querySelector("#taskDiv").innerHTML += `<p id = "#task${task.id}"> <input type="checkbox" class = "check" >${task.description} <button class = "Delete">delete</button></p>`;
+        }
+    });
+
+    document.querySelectorAll(".Delete").forEach(btn => {
+        btn.addEventListener('click', deleteTask);
+    });
+    document.querySelectorAll(".check").forEach(input => {
+        input.addEventListener("change", underlineTask)
+    });
+}
+
 function makeUI() {
 
     // Crear elementos para pintar UI
