8710c93fd3a9f9e7c036569819945c61fb9fe85a
McDave18 <david.huerta.b18@gmail.com>
2021-02-06 17:01:20 -0600

    Agregando estilos a las tareas

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,3 +1,65 @@
+// Task prototype 
+
+function Task(id, desc, completed) {
+
+    this.id = id;
+    this.desc = desc;
+    this.completed = completed;
+}
+
+// functions
+
+
+function getTasks() {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    return tasks;
+}
+
+function updateTasks(newTasks) {
+    localStorage.setItem("tasks", JSON.stringify(newTasks));
+    printTasks(newTasks);
+}
+
+function printTasks(tasks) {
+    document.querySelector("#taskDiv").innerHTML = "";
+    tasks.forEach(task => {
+        console.log(task)
+        if (task.completed) {
+            document.querySelector("#taskDiv").innerHTML += `<p id = "#task${task.id}" style = "text-decoration: line-through; display: flex; justify-content: center; margin: 25px"> <input type="checkbox" class = "check" checked >${task.desc} <button class = "Delete" style= "border-radius: 2em; background-color: #a00000; color: white; width: 60px; border-color: white">delete</button></p>`;
+        } else {
+            document.querySelector("#taskDiv").innerHTML += `<p id = "#task${task.id}" style = "display: flex; justify-content: center; margin: 25px;"> <input type="checkbox" class = "check" >${task.desc} <button class = "Delete" style= "background-color: #a00000; border-radius: 2em; color: white; width: 60px; border-color: white;">delete</button></p>`;
+        }
+
+
+    });
+
+    document.querySelectorAll(".Delete").forEach(btn => {
+        btn.addEventListener('click', deleteTask);
+    });
+    document.querySelectorAll(".check").forEach(input => {
+        input.addEventListener("change", underlineTask)
+    });
+
+
+}
+
+function addTask() {
+
+    let tasks = getTasks();
+    const taskdesc = document.querySelector("#task").value;
+    if (!taskdesc.trim()) {
+        return alert('add a task');
+    }
+    const task = new Task(tasks.length, taskdesc, false);
+    tasks.push(task);
+    updateTasks(tasks);
+    document.querySelector("#task").value = "";
+    printTasks(tasks);
+
+}
+
+
 function deleteTask(btn) {
     const idbutton =btn.srcElement.parentNode.id; //se almacena en una variable el id del elemento
     const tasks = getTasks();   //Se traen todos los elementos
@@ -5,12 +67,12 @@ function deleteTask(btn) {
     updateTasks(filteredTasks); //Se actualiza la lista
 }
 
+
 function underlineTask(input) {
     let tasks = getTasks().map(task => {
-        if (input.target.parentNode.id == `#task${task.id}`)
-            task.completed = !task.completed;
+        if (input.target.parentNode.id == `#task${task.id}`){
+            task.completed = !task.completed;}
         return task;
     });
     updateTasks(tasks);
 }
-
